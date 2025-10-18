a3933c5a5e937185c5ff9474f758db25821ae97c
akbar-farajov <akbarfarajov74@gmail.com>
2025-10-19 00:12:10 +0400

    Fix layout height class in RootLayout component for correct screen adaptation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="max-h-[100dvh] h-[100dvh] flex flex-col">
+          <div className="max-h-scren h-screen flex flex-col">
             <Navbar />
             <main className="flex-1">{children}</main>
             <BackgroundAnimation />
