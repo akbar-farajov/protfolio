3c8031ec8afab2fb62d992271b2996b20b0b8175
akbar-farajov <akbarfarajov74@gmail.com>
2025-10-18 22:18:01 +0400

    Update Supabase client configuration and adjust image paths in seed data
    - Changed the Supabase client initialization to use NEXT_PUBLIC_SUPABASE_ANON_KEY instead of NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY for improved security.
    - Modified image paths in the seed.sql file to remove the '/images' prefix, ensuring correct asset loading for project entries.

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -8,7 +8,7 @@ INSERT INTO public.projects (title, description, tags, image, live_url, github_u
   'Turbotable AI',
   'A no-code / low-code automation platform that helps convert documents (PDFs, forms, contracts, receipts, even handwriting) into structured data.It can also act as an AI assistant for analyzing data, building automations, creating charts, etc.',
   ARRAY['Next.js', 'TypeScript', 'Supabase', 'Vercel AI SDK'],
-  '/images/turbotable.png',
+  '/turbotable.png',
   'https://turbotable.ai/',
   NULL
 ),
@@ -16,7 +16,7 @@ INSERT INTO public.projects (title, description, tags, image, live_url, github_u
   'Azeristudent',
   'AzeriStudent is an educational consulting / counselling agency based in Baku, Azerbaijan. Their focus is helping students who want to study abroad—undergraduate, graduate, language schools, etc.',
   ARRAY['Next.js', 'TypeScript', 'Strapi CMS', 'Tailwind CSS', 'Vercel AI SDK'],
-  '/images/azeristudent.png',
+  '/azeristudent.png',
   'https://azeristudent.az/',
   NULL
 ),
@@ -24,7 +24,7 @@ INSERT INTO public.projects (title, description, tags, image, live_url, github_u
   'DigSME',
   'DigSME is a platform aimed at helping micro, small, and medium-sized enterprises (SMEs) assess their digital maturity — i.e. how well they''re using digital tools, processes, and technologies.',
   ARRAY['Next.js', 'TypeScript', 'Supabase', 'Zustand', 'Tailwind CSS'],
-  '/images/digsme.png',
+  '/digsme.png',
   'https://digsme.org.ua/',
   NULL
 ),
@@ -32,7 +32,7 @@ INSERT INTO public.projects (title, description, tags, image, live_url, github_u
   'TagMe',
   'TagMe is a modern social media platform inspired by Instagram, allowing users to share photos, interact, and connect with others.',
   ARRAY['Next.js', 'TypeScript', 'Tailwind CSS', 'Supabase', 'Shadcn UI'],
-  '/images/tagme.png',
+  '/tagme.png',
   'https://tagme.vercel.app',
   'https://github.com/akbar-farajov/tagme'
 ),
@@ -40,7 +40,7 @@ INSERT INTO public.projects (title, description, tags, image, live_url, github_u
   'Ecommerce Store',
   'A modern ecommerce store built with Next.js, TypeScript, and Tailwind CSS, featuring a sleek UI and smooth user experience.',
   ARRAY['Next.js', 'TypeScript', 'Tailwind CSS', 'Wix API', 'Zustand'],
-  '/images/ecommerce-app.png',
+  '/ecommerce-app.png',
   'https://ecommerce-smoky-eta-57.vercel.app/',
   'https://github.com/akbar-farajov/ecommerce'
 );
